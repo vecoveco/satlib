cedc655001aad80086009593866383938b9b4b43
Velibor Pejcic <vecoveco@users.noreply.github.com>
2020-03-11 08:36:06 +0100

    Add
    add dpr PrecipWarterPath

--- satlib/vis.py
+++ satlib/vis.py
@@ -42,3 +42,70 @@ def plot_gprof_IWP(gprof_path):
     plt.yticks(fontsize=20)
     plt.show()
     
+def plot_dpr_PWI(gprof_path):
+    """
+    dpr_path ::: path to DPR data
+    
+    """
+    
+    gpmdprs = h5py.File(pfad, 'r')
+    gpm_lat = np.array(gpmdprs['NS']['Latitude'])
+    gpm_lon = np.array(gpmdprs['NS']['Longitude'])
+
+    gpm_pwi = np.array(gpmdprs['NS']['SLV']['precipWaterIntegrated']) # in g/m^2
+    print(gpm_pwi.shape)
+    # Missing Values
+    gpm_pwi[gpm_pwi==-9999.9]=np.nan
+    
+    # 0 = liquid, sum of liquid water phase >=200
+    # 1 = solid, sum of non liquid water phase < 200
+    gpm_lwi = gpm_pwi[:,:,0]
+    gpm_swi = gpm_pwi[:,:,1]
+    
+    
+    print (gpm_lwi.shape, gpm_lat.shape)
+    
+    # BoxPol Limits
+    xlim = np.array([6.4, 8.1])
+    ylim = np.array([49.9, 51.4])
+
+    # g -> lg
+    kg = 1000
+    
+    plt.figure(figsize=(14,6))
+    plt.subplot(1,2,1)
+    plt.pcolormesh(gpm_lon, gpm_lat, gpm_lwi/kg,
+                   vmin=0, vmax=2,
+                   cmap=mpl.cm.get_cmap('jet', 10))
+    
+    cbar = plt.colorbar()
+    cbar.set_label(r'Liquid Water Integrated $g/m^2$', fontsize=20)
+    cbar.ax.tick_params(labelsize=20)
+    plt.scatter(7.071663, 50.73052,c='magenta',s=50, marker='+', label='BoXPol')
+    plt.legend(fontsize=20)
+    plt.xlim(xlim[0], xlim[1])
+    plt.ylim(ylim[0], ylim[1])
+    plt.xlabel('Longitude in deg', fontsize=20)
+    plt.ylabel('Latitude in deg', fontsize=20)
+    plt.xticks(fontsize=20)
+    plt.yticks(fontsize=20)
+    
+    plt.subplot(1,2,2)
+    plt.pcolormesh(gpm_lon, gpm_lat, gpm_swi/kg,
+                   vmin=0, vmax=4,
+                   cmap=mpl.cm.get_cmap('jet', 10))
+    
+    cbar = plt.colorbar()
+    cbar.set_label(r'Non Liquid Water Integrated $g/m^2$', fontsize=20)
+    cbar.ax.tick_params(labelsize=20)
+    plt.scatter(7.071663, 50.73052,c='magenta',s=50, marker='+', label='BoXPol')
+    plt.legend(fontsize=20)
+    plt.xlim(xlim[0], xlim[1])
+    plt.ylim(ylim[0], ylim[1])
+    plt.xlabel('Longitude in deg', fontsize=20)
+    plt.ylabel('Latitude in deg', fontsize=20)
+    plt.xticks(fontsize=20)
+    plt.yticks(fontsize=20)
+    
+    plt.tight_layout()
+    plt.show()
